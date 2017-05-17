530ba805c1f0932f454c1ac8b55b35cf73fa877d
kaliduude <kaliduude@gmail.com>
2017-05-17 19:19:35 +0300

    Add files via upload

---
+++ Http.bat
@@ -0,0 +1,5 @@
+@echo off
+title Proxy Scaper By Kali
+node proxies.js -H
+echo GoodBye !
+pause >nul
---
+++ Socks5.bat
@@ -0,0 +1,5 @@
+@echo off
+title Proxy Scaper By Kali
+node proxies.js -s
+echo Done!
+pause >nul
